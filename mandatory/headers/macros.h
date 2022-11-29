49b0737922b8f999bc141ddf8ab3900792e60db2
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-11-29 15:14:39 +0100

    delete unused colors in macros.h

--- mandatory/headers/macros.h
+++ mandatory/headers/macros.h
@@ -6,22 +6,14 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 10:24:24 by bbrahim           #+#    #+#             */
-/*   Updated: 2022/11/25 19:40:51 by bbrahim          ###   ########.fr       */
+/*   Updated: 2022/11/29 15:14:10 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MACROS_H
 # define MACROS_H
 
-# define BLD "\033[31;0m\033[1m"
-# define NC "\033[31;0m"
 # define RED "\033[31;0m\033[0;31;1m"
-# define YEL "\033[31;0m\033[0;33;1m"
-# define GRA "\033[31;0m\033[0;37;1m"
-# define CYN "\033[31;0m\033[0;36;1m"
-# define GRN "\033[31;0m\033[0;32;1m"
-# define MGN "\033[31;0m\033[0;35;1m"
-# define BLU "\033[31;0m\033[0;34;1m"
 
 # define BUFFER_SIZE 1
 # define WIN_WIDTH 800
